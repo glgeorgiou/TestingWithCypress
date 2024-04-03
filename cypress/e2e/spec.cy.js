100b2d17bb1e02a54f4c7f6296db5f738bd564e3
George <ggeorgiou.gr@gmail.com>
2024-04-03 18:46:09 +0300

    Practicing with Cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
 describe('Open a web page', () => {
   it('Visit my cv site', () => {
     cy.visit('https://www.ggeorgiou.gr')
+
+    // Get an input, type into it
+    cy.get('#s').type('wordpress')
+
+    //  Verify that the value has been updated
+    cy.get('#s').should('have.value', 'wordpress')
+
+    // Search results
+    cy.get('.searchsubmit').click()
   })
 })
